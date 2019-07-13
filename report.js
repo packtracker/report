b6b2fdfc13d00e48ab6e1c2b7a91d4bbc3950b79
Jonathan Johnson <me@jondavidjohn.com>
2019-07-12 22:34:44 -0700

    changes to decompose dockerfile and support circle orbs

--- report.js
+++ report.js
@@ -2,28 +2,19 @@ const util = require('util')
 const fs = require('fs')
 const webpack = require('webpack')
 const PacktrackerPlugin = require('@packtracker/webpack-plugin')
-const event = require(process.env.GITHUB_EVENT_PATH)
-
-console.log(util.inspect(event))
 
 let config = {}
 if (fs.lstatSync(process.env.WEBPACK_CONFIG_PATH).isFile()) {
   config = require(process.env.WEBPACK_CONFIG_PATH)
 }
 
-const pt_config = {
-  upload: true,
-  fail_build: true,
-  branch: event.ref.replace('refs/heads/', ''),
-  author: event.head_commit.author.email,
-  message: event.head_commit.message,
-  commit: process.env.GITHUB_SHA,
-  committed_at: parseInt(+new Date(event.head_commit.timestamp) / 1000),
-  prior_commit: event.before,
-}
-
 config.plugins = config.plugins || []
-config.plugins.push(new PacktrackerPlugin(pt_config))
+
+if (process.env.GITHUB_EVENT_PATH) {
+  config.plugins.push(new PacktrackerPlugin(githubConfig()))
+} else if (process.env.CIRCLECI) {
+  config.plugins.push(new PacktrackerPlugin(circleConfig()))
+}
 
 webpack(config, (err) => {
   if (err) {
@@ -32,3 +23,27 @@ webpack(config, (err) => {
     process.exit(0)
   }
 })
+
+function githubConfig () {
+  const event = require(process.env.GITHUB_EVENT_PATH)
+  console.log(util.inspect(event))
+  return {
+    upload: true,
+    fail_build: true,
+    branch: event.ref.replace('refs/heads/', ''),
+    author: event.head_commit.author.email,
+    message: event.head_commit.message,
+    commit: process.env.GITHUB_SHA,
+    committed_at: parseInt(+new Date(event.head_commit.timestamp) / 1000),
+    prior_commit: event.before,
+  }
+}
+
+function circleConfig () {
+  return {
+    upload: true,
+    fail_build: true,
+    branch: process.env.CIRCLE_BRANCH,
+    commit: process.env.CIRCLE_SHA1,
+  }
+}
