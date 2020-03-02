d7a89e47d52adef7d4d82c70405f90b4895dc8e7
Jonathan Johnson <me@jondavidjohn.com>
2020-03-02 11:44:13 -0600

    update version releases

--- publish.sh
+++ publish.sh
@@ -1,8 +1,31 @@
+#!/bin/sh
+
+set -eu
+
 [[ -z "$1" ]] && { echo "No version specified" ; exit 1; }
 version="$1"
 
 [[ -z "$2" ]] && { echo "No environment specified, defaulting to development"; }
 environment="${2:-development}"
+
+
+if [ "$environment" == "production" ]; then
+cat <<-VERSIONING
+  It looks like you're trying to publish to production!
+
+  Be sure that you have bumped the version in the following places:
+    - Dockerfile
+    - orb.yml
+    - README.md
+
+VERSIONING
+
+  read -p "Ready to publish? " -n 1 -r
+  echo    # (optional) move to a new line
+  if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+fi
 
 if [ "$environment" == "production" ]; then
   dockertag="packtracker/report:$version"
